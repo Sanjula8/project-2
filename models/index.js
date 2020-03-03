7e09249a53ab110ca71a9a681aa87889f6a8cebb
Sanjula <sanjula8@gmail.com>
2020-03-03 10:06:32 -0600

    formatting all code iwth eslint

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 "use strict";
 //empty comment
+var sequelize;
 
 var fs = require("fs");
 var path = require("path");
@@ -10,33 +11,33 @@ var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-	var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-	var sequelize = new Sequelize(
-		config.database,
-		config.username,
-		config.password,
-		config
-	);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs.readdirSync(__dirname)
-	.filter(function(file) {
-		return (
-			file.indexOf(".") !== 0 &&
+  .filter(function(file) {
+    return (
+      file.indexOf(".") !== 0 &&
 			file !== basename &&
 			file.slice(-3) === ".js"
-		);
-	})
-	.forEach(function(file) {
-		var model = sequelize["import"](path.join(__dirname, file));
-		db[model.name] = model;
-	});
+    );
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(function(modelName) {
-	if (db[modelName].associate) {
-		db[modelName].associate(db);
-	}
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
