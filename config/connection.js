96fea190d0451b3f591cf2d8ec33f6b031fc9df9
Brett Sevy <justbrettt@gmail.com>
2020-03-06 08:22:24 -0600

    added routes to stocks and added to handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "root",
   database: "stocks_db"
 });
 
